2e1e5b1984e82e387205187bf3bae7341019c18b
Duy <duynguyenngoc@hotmail.com>
2021-05-11 17:30:48 +0700

    new feauture

--- identity_card_service.py
+++ identity_card_service.py
@@ -32,22 +32,25 @@ def make_string_now_time():
         else:
             day = str(now.day)
         return str(now.year) + '-' + month + '-' + day
-    
+
+
 def job():
     print('start')
-    res = requests.get('http://{host}:{port}/api/v1/ml-split/identity-card/import'.format(host=config.BE_HOST, port = config.BE_PORT))
+    res = requests.get('http://{host}:{port}/api/v1/ml-split/identity-card/100'.format(host=config.BE_HOST, port = config.BE_PORT))
+    print('load all identity card import status 100', res.status_code)
     if res.status_code != 200:
+        print(res.json())
         return None
     imports= res.json()
     for item in imports:
-        if item.url == None:
+        if item['url'] == None:
             continue
         try:
-            image = image_utils.read_bytes_image_from_url(item.url)
+            image = image_utils.read_bytes_image_from_url(item['url'])
             image = np.array(Image.open(io.BytesIO(image)))
             img_crop = image_utils.crop_image(image)
             im_height, im_width, _ = img_crop.shape
-            name_doc = item.name.split('.')[0]
+            name_doc = item['name'].split('.')[0]
             id_boxes, name_boxes, birth_boxes, home_boxes, add_boxes, category_index = model.detect_text_cmnd(img_crop)
             img_id_boxes = image_utils.handle_detection(id_boxes, img_crop)
             img_name_boxes = image_utils.handle_detection(name_boxes, img_crop)
@@ -55,43 +58,43 @@ def job():
             img_add_boxes = image_utils.handle_detection(add_boxes, img_crop)
             img_home_boxes = image_utils.handle_detection(home_boxes, img_crop)
         except Exception as e:
-            print(e)
+            print('[error] when extract field with: ',e)
             continue
 
         list_fields = ['address', 'id', 'home_town', 'name', 'birthday', 'crop_image']
-
-        for k in len_list_fields:
+        len_list_fields = len(list_fields)
+        for k in range(len_list_fields):
             field_name = list_fields[k]
-            
-            if k == list_fields:
+            print('[run] upload with field: ', field_name)
+            if k == len_list_fields - 1:
                 fields = {
-                    'name': item.name.split('.')[0] + '_' + field_name + '.png',
-                    'document_id': item.id,
+                    'name': item['name'].split('.')[0] + '_' + field_name + '.png',
+                    'document_id': item['id'],
                 }
-                files =  {'image': (item.name.split('.')[0] + '_' + field_name + '.png', image, 'image/png')}
+                files =  {'image': (item['name'].split('.')[0] + '_' + field_name + '.png', image, 'image/png')}
                 r = requests.post(
                     'http://{host}:{port}/api/v1/ftp/image/document-crop'.format(host=config.BE_HOST, port = config.BE_PORT),
-                    files=files, 
-                    data=fields, 
+                    files=files,
+                    data=fields,
                     verify=False
                 )
+                print(r.status_code)
+                print(r.json())
                 break
-                
-            
             fields = {
-                'name': item.name.split('.')[0] + '_' + field_name + '.png',
-                'document_id': item.id,
+                'name': item['name'].split('.')[0] + '_' + field_name + '.png',
+                'document_id': item['id'],
                 'field_name': field_name
             }
-            files =  {'image': (item.name.split('.')[0] + '_' + field_name + '.png', image, 'image/png')}
+            files =  {'image': (item['name'].split('.')[0] + '_' + field_name + '.png', image, 'image/png')}
             r = requests.post(
                 'http://{host}:{port}/api/v1/ftp/image/split'.format(host=config.BE_HOST, port = config.BE_PORT),
-                files=files, 
-                data=fields, 
+                files=files,
+                data=fields,
                 verify=False
             )
-        
-schedule.every(20).seconds.do(job)
+            print(r.status_code)
+schedule.every(1).seconds.do(job)
 while True:
     schedule.run_pending()
     time.sleep(10)
